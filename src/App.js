3ca3e6d81ae3f7bed0114e3adfb262f060340a13
Kristian Lopez <kristian.lom@gmail.com>
2019-07-23 17:37:45 -0500

    Restricciones en JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import "./styles/styles.scss";
 
-const App = () => <h1>Hola Mundo</h1>
+const App = () => (
+  <>
+    <div className="saludo">
+      <h1>Hola Mundo</h1>
+      <p>Hola, saludando desde mi componente.</p>
+    </div>
+    <div>
+      <img src="https://ed-grid.com/assets/img/EDgrid-logo.svg"></img>
+    </div>
+  </>
+)
 
 export default App;
+
+
+// Reglas JSX
+// 1: Toda etiqueta debe cerrarse.
+// 2: Los componentes deben devolver un solo elemento padre.
+// 3: Apoyarse de los Fragment cuando necesito devolver 2 elementos
+// 4: Fragment => <> hijos </>
+// 5: img siempre se cierra.
+// 6: class => className.
+// 7: for => htmlFor
+// NO if, else, while
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
+import App from './App';
 
 const root = document.getElementById("root");
-//const elemento = React.createElement(componente, propiedades, hijos);
-const elemento = React.createElement("H1", {className: "saludo"}, "Hola Mundo");
 
-ReactDOM.render(elemento, root);
+ReactDOM.render(<App />, root);
