2f4119ed231727f59d59b3f737f22b8c1cf8a599
Kristian Lopez <kristian.lom@gmail.com>
2019-07-24 16:51:14 -0500

    Creación de un Formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -10,6 +10,19 @@ class Formulario extends Component {
         return (
             <div className="ed-grid">
                 <h1>Formulario</h1>
+                <form>
+                    <div className="form__item">
+                        <label htmlFor="nombre">Nombre Completo</label>
+                        <input id="nombre" type="text" />
+                    </div>
+                    <div className="form__item">
+                        <label htmlFor="email">Correo Electrónico</label>
+                        <input id="email" type="email" />
+                    </div>
+                    <div className="form__item">
+                        <input className="button full" type="submit" value="Enviar" />
+                    </div>
+                </form>
             </div>
         )
     }
