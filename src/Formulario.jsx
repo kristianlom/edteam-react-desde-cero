d2da18f5caa95d36160cc44390efc7bde9000606
Kristian Lopez <kristian.lom@gmail.com>
2019-07-24 22:09:27 -0500

    Ciclo de vida de desmontaje

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -7,10 +7,12 @@ class Formulario extends Component {
 
         this.state = {
             nombre: "",
-            email: ""
+            email: "",
+            fecha: new Date()
         }
         this.cambiarNombre = this.cambiarNombre.bind(this)
         this.cambiarEMail = this.cambiarEMail.bind(this)
+        this.cambiarFecha = this.cambiarFecha.bind(this)
     }
 
     cambiarNombre(e) {
@@ -25,11 +27,17 @@ class Formulario extends Component {
         })
     }
 
+    cambiarFecha() {
+        this.setState({
+            fecha: new Date()
+        })
+    }
     render() {
 
         return (
             <div className="ed-grid">
                 <h1>Formulario</h1>
+                <h4> Fecha actual : {Math.ceil(this.state.fecha / 1000)}</h4>
                 <form id="elemento">
                     <div className="ed-grid m-grid-2">
                         <div className="form__item">
@@ -58,15 +66,19 @@ class Formulario extends Component {
 
     componentDidMount() {
         let elemento = document.getElementById("elemento")
-        console.log(elemento)
+        this.intervaloFecha = setInterval(() => {
+            this.cambiarFecha()
+            console.log(new Date())
+        }, 1000)
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log(prevProps)
-        console.log(prevState)
-        console.log("-----------------")
+        //console.log(prevState)
     }
 
+    componentWillUnmount() {
+        clearInterval(this.intervaloFecha)
+    }
 }
 
 
