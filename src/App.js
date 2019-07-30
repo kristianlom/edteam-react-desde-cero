d93f546bbfe4c2a3edd0fb42b08c3a10045bfd67
Kristian Lopez <kristian.lom@gmail.com>
2019-07-30 13:40:21 -0500

    Declarando Router y Rutas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import "./styles/styles.scss";
 import Banner from "./Banner"
 import Formulario from "./Formulario"
-import CourseGrid from './CourseGrid';
+import CourseGrid from './CourseGrid'
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 const App = () => (
-    <>
-        <Banner />
-        <CourseGrid />
-    </>
+    <Router>
+        <Route path="/" exact component={Banner} />
+        <Route path="/cursos" exact component={CourseGrid} />
+        <Route path="/formulario" exact component={() => <Formulario name="Página de contacto" />} />
+        <Route component={()=> (
+            <div className="ed-grid">
+                <h1>Error 404</h1>
+                <span>Página no encontrada</span>
+            </div>
+        )}/>
+    </Router>
 )
 
 export default App;
