1b9a4e4531e2322f551173f6456c57e13d74214c
Kristian Lopez <kristian.lom@gmail.com>
2019-07-30 13:21:47 -0500

    Resumen del primer curso, se crea componente de los cursos a mostrar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import "./styles/styles.scss";
 import Banner from "./Banner"
 import Formulario from "./Formulario"
+import CourseGrid from './CourseGrid';
 
 const App = () => (
     <>
         <Banner />
-        <Formulario />
+        <CourseGrid />
     </>
 )
 
