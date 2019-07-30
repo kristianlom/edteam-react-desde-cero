48330bcca47df543b151ffc1a250f2de59c05693
Kristian Lopez <kristian.lom@gmail.com>
2019-07-30 13:45:38 -0500

    Se agrega Switch y página 404

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./styles/styles.scss";
 import Banner from "./Banner"
 import Formulario from "./Formulario"
 import CourseGrid from './CourseGrid'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 const App = () => (
     <Router>
-        <Route path="/" exact component={Banner} />
-        <Route path="/cursos" exact component={CourseGrid} />
-        <Route path="/formulario" exact component={() => <Formulario name="Página de contacto" />} />
-        <Route component={()=> (
-            <div className="ed-grid">
-                <h1>Error 404</h1>
-                <span>Página no encontrada</span>
-            </div>
-        )}/>
+        <Switch>
+            <Route path="/" exact component={Banner} />
+            <Route path="/cursos" component={CourseGrid} />
+            <Route path="/cursos/:id" component={Course} />
+            <Route path="/formulario" component={() => <Formulario name="Página de contacto" />} />
+            <Route component={() => (
+                <div className="ed-grid">
+                    <h1>Error 404</h1>
+                    <span>Página no encontrada</span>
+                </div>
+            )} />
+        </Switch>
     </Router>
 )
 
