f2e13f579fb6c9435c2c0c12443b872d2054ef88
Kristian Lopez <kristian.lom@gmail.com>
2019-08-02 10:30:36 -0500

    Actualizando el estado con Hooks

--- src/Components/Pages/Courses.jsx
+++ src/Components/Pages/Courses.jsx
@@ -3,25 +3,30 @@ import React, {useState} from "react"
 const Course = ({match}) => {
 
     const [state, setState] = useState({
-        currentCourse: {
-            "id": 1,
-            "title": "React desde cero",
-            "image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
-            "price": 40,
-            "professor": "Beto Quiroga"
-        }
+        id: 1,
+        title: "React desde cero",
+        image: "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+        price: 40,
+        professor: "Beto Quiroga"
     });
 
-    console.log(state);
+    const changeTitle = (text) => {
+        setState({
+                ...state,
+                title: text
+            }
+        )
+    }
 
     return (
         <div className="ed-grid m-grid-3"> {
-            state.currentCourse
+            state
                 ?
                 (<>
-                    <h1 className="m-cols-3">{state.currentCourse.title}</h1>
-                    <img className="m-cols-1" src={state.currentCourse.image} alt={state.currentCourse.title}/>
-                    <p className="m-cols-2">Profesor : {state.currentCourse.professor}</p>
+                    <h1 className="m-cols-3">{state.title}</h1>
+                    <img className="m-cols-1" src={state.image} alt={state.title}/>
+                    <p className="m-cols-2">Profesor : {state.professor}</p>
+                    <button onClick={changeTitle.bind(this, "Go desde cero")}>Cambiar titutlo</button>
                 </>)
                 :
                 <h1>El curso no existe</h1>
