2eda3ddb75c2b85ed3f5e5ed3372872d2f347246
Kristian Lopez <kristian.lom@gmail.com>
2019-08-02 11:10:20 -0500

    Componentes con multiples estados.

--- src/Components/Pages/Course.jsx
+++ src/Components/Pages/Course.jsx
@@ -3,24 +3,36 @@ import axios from "axios";
 
 const Course = ({match}) => {
 
-    const [state, setState] = useState({});
+    const [course, setCourse] = useState({});
+    const [comment, setComment] = useState("Sin comentarios");
 
     useEffect(() => {
         axios.get(`http://my-json-server.typicode.com/kristianlom/edteam-json-db/courses/${match.params.id}`)
-            .then(resp => setState(
+            .then(resp => setCourse(
                 resp.data
             ))
     }, []);
 
+    const myComment = e => {
+        setComment(e.target.value)
+    }
+
     return (
         <div className="ed-grid m-grid-3"> {
-            state
+            course
                 ?
-                (<>
-                    <h1 className="m-cols-3">{state.title}</h1>
-                    <img className="m-cols-1" src={state.image} alt={state.title}/>
-                    <p className="m-cols-2">Profesor : {state.professor}</p>
-                </>)
+                (<div className="ed-grid">
+                    <div className="l-block">
+                        <h1 className="m-cols-3">{course.title}</h1>
+                        <img className="m-cols-1" src={course.image} alt={course.title}/>
+                        <p className="m-cols-2">Profesor : {course.professor}</p>
+                    </div>
+                    <div className="ed-grid">
+                        <h2>Escribe tu comentario</h2>
+                        <input type="text" placeholder="Escribe ... " onChange={myComment.bind(this)}/>
+                        <p>{comment}</p>
+                    </div>
+                </div>)
                 :
                 <h1>El curso no existe</h1>
         }
