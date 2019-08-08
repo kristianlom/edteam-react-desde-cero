d87ac8b145abcf5d3f355a30d7fbdbb4dc7f0f8a
Kristian Lopez <kristian.lom@gmail.com>
2019-08-08 18:54:26 -0500

    Conceptos de Redux

---
+++ src/Components/AppRoutes.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import Form from "./Pages/Form"
+import Courses from './Pages/Courses'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Course from "./Pages/Course"
+import MainMenu from './Organisms/MainMenu'
+import History from './Pages/History'
+import Home from "../Components/Pages/Home";
+import Users from "./Pages/Users";
+
+const AppRoutes = () => (
+    <Router>
+        <MainMenu/>
+        <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/courses/:id" component={Course}/>
+            <Route path="/courses" component={Courses}/>
+            <Route path="/history/:value" component={History}/>
+            <Route path="/history" component={History}/>
+            <Route path="/users" component={Users}/>
+            <Route path="/form" component={() => <Form name="Página de contacto"/>}/>
+            <Route component={() => (
+                <div className="ed-grid">
+                    <h1>Error 404</h1>
+                    <span>Página no encontrada</span>
+                </div>
+            )}/>
+        </Switch>
+    </Router>
+)
+
+export default AppRoutes
