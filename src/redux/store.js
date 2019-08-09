edc4eba08cc2b310ca11f3c0eb537178d210efc0
Kristian Lopez <kristian.lom@gmail.com>
2019-08-08 20:47:47 -0500

    Modificando el estado global

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
 import {createStore} from "redux";
+import {ADD_TO_CART} from "./actions";
 
 const initialStore = {
     cart: []
 };
 
 const rootReducer = (state = initialStore, action) => {
-    console.log(action)
+    if(action.type === ADD_TO_CART) {
+        return {
+            ...state,
+            cart: state.cart.concat(action.id)
+        }
+    }
+
     return state
 };
 
